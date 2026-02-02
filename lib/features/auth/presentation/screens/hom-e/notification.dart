e3329551030d9748e8ca9206c0429837df87c681
shaimaa mohamed <mhmdbdalshyma@gmail.com>
2026-02-02 16:38:47 +0200

    Remove commented code for notifications area
    Removed commented-out code for notifications section.

--- lib/features/auth/presentation/screens/hom-e/notification.dart
+++ lib/features/auth/presentation/screens/hom-e/notification.dart
@@ -52,7 +52,6 @@ class NotificationScreen extends StatelessWidget {
                 ),
               ),
 
-              // المنطقة البيضاء (قائمة التنبيهات)
               Expanded(
                 child: Container(
                   width: double.infinity,
